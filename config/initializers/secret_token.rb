16a756cca44aaa73d2ae745f2ada13564c8d4bc4
Bragaadeesh <jbragadeesh@yahoo.com>
2012-04-14 23:47:00 +0530

    First commit for the btob project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Btob::Application.config.secret_token = 'b4d1c40182aa1ad370c7d9a31db9fa0de66fdfe4ad876c0743ca345584f146f383f89cab134adc23be065160e499791abdbd831d28887a2c446885a10376dcc3'
