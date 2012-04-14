186fd79582155e592c5cc04e9a7e6c3557b9fd5e
Bragaadeesh <jbragadeesh@yahoo.com>
2012-04-15 01:50:36 +0530

    Try another combination for production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,9 @@
 //= require underscore
 //= require backbone
 //= require .//btob
-//= require_tree ../templates/
+//= require_tree ../templates
 //= require_tree .//models
 //= require_tree .//collections
 //= require_tree .//views
 //= require_tree .//routers
-//= require_tree ../javascripts
+//= require_tree .
